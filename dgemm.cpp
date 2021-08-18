1cd4794c745eec26cb29c34caf7396c3f843826a
Tom Deakin <tom.deakin@bristol.ac.uk>
2021-08-18 14:14:48 +0100

    Add blocked nd_range version

--- dgemm.cpp
+++ dgemm.cpp
@@ -20,6 +20,8 @@ const double Bval = 5.0;
 void init_input_matrices(sycl::queue& Q, const size_t Ndim, const size_t Mdim, const size_t Pdim, sycl::buffer<double,2>& A, sycl::buffer<double,2>& B);
 void zero_matrix(sycl::queue& Q, const size_t Ndim, const size_t Mdim, sycl::buffer<double,2>& C);
 void matmul(sycl::queue& Q, const size_t Ndim, const size_t Mdim, const size_t Pdim, sycl::buffer<double,2>& A, sycl::buffer<double,2>& B, sycl::buffer<double,2>& C);
+void matmul_blocked(sycl::queue& Q, const size_t Ndim, const size_t Mdim, const size_t Pdim, sycl::buffer<double,2>& A, sycl::buffer<double,2>& B, sycl::buffer<double,2>& C);
+void matmul_hipar(sycl::queue& Q, const size_t Ndim, const size_t Mdim, const size_t Pdim, sycl::buffer<double,2>& A, sycl::buffer<double,2>& B, sycl::buffer<double,2>& C);
 void get_true_solution(const int Ndim, const int Mdim, const int Pdim, double * C);
 double error(const int Ndim, const int Mdim, double * C, double * Cgold);
 
@@ -61,34 +63,108 @@ int main(int argc, char *argv[]) {
   sycl::buffer<double, 2> C({Ndim,Mdim});
 
   double *Cgold = new double[Ndim*Mdim];
+  get_true_solution(Ndim, Mdim, Pdim, Cgold);
 
   init_input_matrices(Q, Ndim, Mdim, Pdim, A, B);
 
-  zero_matrix(Q, Ndim, Mdim, C);
 
-  // Make sure previous work finished for accurate timing
-  Q.wait_and_throw();
+  //
+  // Initial version
+  //
+  {
+    std::cout << "  Simple version:" << std::endl << std::endl;
 
-  auto tic = clock::now();
-  matmul(Q, Ndim, Mdim, Pdim, A, B, C);
-  Q.wait_and_throw();
-  auto toc = clock::now();
+    // Make sure previous work finished for accurate timing
+    zero_matrix(Q, Ndim, Mdim, C);
+    Q.wait_and_throw();
 
-  get_true_solution(Ndim, Mdim, Pdim, Cgold);
+    auto tic = clock::now();
+    matmul(Q, Ndim, Mdim, Pdim, A, B, C);
+    auto toc = clock::now();
 
-  double err = error(Ndim, Mdim, C.get_access<sycl::access_mode::read>().get_pointer(), Cgold);
 
-  if (err < 1.0E-8) {
-    std::cout << "  Solution correct" << std::endl;
-  } else {
+    double err = error(Ndim, Mdim, C.get_access<sycl::access_mode::read>().get_pointer(), Cgold);
+
+    if (err < 1.0E-8) {
+      std::cout << "  Solution correct" << std::endl;
+    } else {
+      std::cout
+        << "  Solution *NOT* correct" << std::endl
+        << "    Error = " << err << std::endl;
+    }
+
+    // Print timings
     std::cout
-      << "  Solution *NOT* correct" << std::endl
-      << "    Error = " << err << std::endl;
+      << "  matmul took " << timing{toc-tic}.count() << " s" << std::endl
+      << "  GFLOP/s: " << 1.0E-9 * 2.0 * Ndim * Mdim * Pdim / (timing{toc-tic}.count()) << std::endl;
+
+    std::cout << "  --------------------------------" << std::endl << std::endl;
   }
 
-  // Print timings
-  std::cout
-    << "  matmul took " << timing{toc-tic}.count() << " s" << std::endl;
+  //
+  // nd_range blocked version
+  //
+  {
+    std::cout << "  Blocked nd_range version:" << std::endl << std::endl;
+
+    // Make sure previous work finished for accurate timing
+    zero_matrix(Q, Ndim, Mdim, C);
+    Q.wait_and_throw();
+
+    auto tic = clock::now();
+    matmul_blocked(Q, Ndim, Mdim, Pdim, A, B, C);
+    auto toc = clock::now();
+
+
+    double err = error(Ndim, Mdim, C.get_access<sycl::access_mode::read>().get_pointer(), Cgold);
+
+    if (err < 1.0E-8) {
+      std::cout << "  Solution correct" << std::endl;
+    } else {
+      std::cout
+        << "  Solution *NOT* correct" << std::endl
+        << "    Error = " << err << std::endl;
+    }
+
+    // Print timings
+    std::cout
+      << "  matmul took " << timing{toc-tic}.count() << " s" << std::endl
+      << "  GFLOP/s: " << 1.0E-9 * 2.0 * Ndim * Mdim * Pdim / (timing{toc-tic}.count()) << std::endl;
+
+    std::cout << "  --------------------------------" << std::endl << std::endl;
+  }
+
+  //
+  // Hierarchical parallel version
+  //
+  {
+    std::cout << "  Hierarchical parallel version:" << std::endl << std::endl;
+
+    zero_matrix(Q, Ndim, Mdim, C);
+    Q.wait_and_throw();
+
+    auto tic = clock::now();
+    matmul_hipar(Q, Ndim, Mdim, Pdim, A, B, C);
+    auto toc = clock::now();
+
+    double err = error(Ndim, Mdim, C.get_access<sycl::access_mode::read>().get_pointer(), Cgold);
+
+    if (err < 1.0E-8) {
+      std::cout << "  Solution correct" << std::endl;
+    } else {
+      std::cout
+        << "  Solution *NOT* correct" << std::endl
+        << "    Error = " << err << std::endl;
+    }
+
+    // Print timings
+    std::cout
+      << "  matmul took " << timing{toc-tic}.count() << " s" << std::endl
+      << "  GFLOP/s: " << 1.0E-9 * 2.0 * Ndim * Mdim * Pdim / (timing{toc-tic}.count()) << std::endl;
+
+    std::cout << "  --------------------------------" << std::endl << std::endl;
+  }
+
 
   // Deallocate memory
   delete[] Cgold;
@@ -158,7 +234,69 @@ void matmul(sycl::queue& Q, const size_t Ndim, const size_t Mdim, const size_t P
         c[idx] += a[i][k] * b[k][j];
       }
     });
-  });
+  }).wait();
+
+}
+
+void matmul_blocked(sycl::queue& Q, const size_t Ndim, const size_t Mdim, const size_t Pdim, sycl::buffer<double,2>& A, sycl::buffer<double,2>& B, sycl::buffer<double,2>& C) {
+
+  const size_t Bsize = 8;
+  assert(Ndim % Bsize == 0);
+  assert(Mdim % Bsize == 0);
+  assert(Pdim % Bsize == 0);
+
+  Q.submit([&](sycl::handler &cgh) {
+    //sycl::accessor a {A, cgh, sycl::read_only};
+    //sycl::accessor b {B, cgh, sycl::read_only};
+    //sycl::accessor c {C, cgh, sycl::read_write};
+    auto a = A.get_access<sycl::access_mode::read>(cgh);
+    auto b = B.get_access<sycl::access_mode::read>(cgh);
+    auto c = C.get_access<sycl::access_mode::read_write>(cgh);
+
+    sycl::accessor<double, 2, sycl::access_mode::read_write, sycl::access::target::local> Awrk({Bsize, Bsize}, cgh);
+    sycl::accessor<double, 2, sycl::access_mode::read_write, sycl::access::target::local> Bwrk({Bsize, Bsize}, cgh);
+
+    cgh.parallel_for(sycl::nd_range<2>{{Ndim, Mdim}, {Bsize, Bsize}}, [=](sycl::nd_item<2> idx) {
+
+      // This work-item will compute C(i,j)
+      const size_t i = idx.get_global_id(0);
+      const size_t j = idx.get_global_id(1);
+
+      // Element C(i,j) is in block C(Iblk, Jblk)
+      const size_t Iblk = idx.get_group(0);
+      const size_t Jblk = idx.get_group(1);
+
+      // C(i,j) is element C(iloc, jloc) of block C(Iblk, Jblk)
+      const size_t iloc = idx.get_local_id(0);
+      const size_t jloc = idx.get_local_id(1);
+
+      // Number of blocks
+      const size_t Nblk = Ndim / Bsize;
+      const size_t Mblk = Mdim / Bsize;
+      const size_t Pblk = Pdim / Bsize;
+
+      for (int Kblk = 0; Kblk < Pblk; ++Kblk) {
+
+        // Copy A and B into local memory
+        Awrk[iloc][jloc] = a[Iblk*Bsize+iloc][Kblk*Bsize+jloc];
+        Bwrk[iloc][jloc] = b[Kblk*Bsize+iloc][Jblk*Bsize+jloc];
+        //sycl::group_barrier(idx.get_group());
+        idx.barrier();
+
+        // Compute matmul for block
+        for (int kloc = 0; kloc < Bsize; ++kloc) {
+          c[i][j] += Awrk[iloc][kloc] * Bwrk[kloc][jloc];
+        }
+        //sycl::group_barrier(idx.get_group());
+        idx.barrier();
+      }
+    });
+  }).wait();
+
+}
+
+void matmul_hipar(sycl::queue& Q, const size_t Ndim, const size_t Mdim, const size_t Pdim, sycl::buffer<double,2>& A, sycl::buffer<double,2>& B, sycl::buffer<double,2>& C) {
+
 
 }
 
